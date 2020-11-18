44f3bd4592fe24962a7008822256e66db7e0bf29
GuSisong <345354880@qq.com>
2020-11-18 14:03:29 +0800

    update

--- InboundSimulation_202021117.py
+++ InboundSimulation_202021117.py
@@ -2,7 +2,7 @@ import pandas as pd
 import math
 
 
-def simulate(data, truck_volume, deviation, site_operation, shuttle, plant_operation, speed_close, speed_far):
+def simulate(data, truck_volume, loading_rate, deviation, site_operation, shuttle, plant_operation, speed_close, speed_far):
     # 流量降序
     data.sort_values(by='日均流量', axis=0, ascending=False, inplace=True)
     result = []
@@ -27,21 +27,23 @@ def simulate(data, truck_volume, deviation, site_operation, shuttle, plant_opera
                     site_volume = float(fil_region[fil_region.提货点 == site]['日均流量'])
                     shift = int(fil_region[fil_region.提货点 == site]['班次'])
                     distance = int(fil_region[fil_region.提货点 == site]['距离'])
+                    vmi = int(fil_region[fil_region.提货点 == site]['是否VMI'])
 
                     # 提货点单班流量大于2车
-                    if site_volume / shift / 2 >= truck_volume:
-                        route_count += 1
-                        route_num = route_count
-                        route_volume = site_volume
+                    if vmi or site_volume / shift / 2 >= truck_volume:
                         trip_round = round(site_volume / shift / truck_volume)
-                        utilization = site_volume / shift / (truck_volume / 0.7 * trip_round)  # 还原到卡车理论容积再运算
-                        if distance <= 50:
-                            trip_time = (distance / speed_close) * 2 + site_operation + plant_operation
-                        else:
-                            trip_time = (distance / speed_far) * 2 + site_operation + plant_operation
+                        if trip_round >= 2:
+                            route_count += 1
+                            route_num = route_count
+                            route_volume = site_volume
+                            utilization = site_volume / shift / (truck_volume / loading_rate * trip_round)  # 还原到卡车理论容积再运算
+                            if distance <= 50:
+                                trip_time = (distance / speed_close) * 2 + site_operation + plant_operation
+                            else:
+                                trip_time = (distance / speed_far) * 2 + site_operation + plant_operation
 
-                        result.append([plant, region, route_num, site, site_volume, route_volume, utilization, trip_round, trip_time])
-                        finish_list.append(site)
+                            result.append([plant, region, route_num, site, site_volume, route_volume, utilization, trip_round, trip_time])
+                            finish_list.append(site)
 
                     else:
                         # 排除VMI和已规划过的提货点
@@ -67,7 +69,7 @@ def simulate(data, truck_volume, deviation, site_operation, shuttle, plant_opera
                                             route_count += 1
                                             route_num = route_count
                                             trip_round = 2
-                                            utilization = route_volume / shift / (truck_volume / 0.7 * trip_round)  # 还原到卡车理论容积再运算
+                                            utilization = route_volume / shift / (truck_volume / loading_rate * trip_round)  # 还原到卡车理论容积再运算
 
                                             result.append([plant, region, route_num, site, site_volume, route_volume, utilization, trip_round, trip_time])
                                             result.append([plant, region, route_num, i, i_volume, route_volume, utilization, trip_round, trip_time])
@@ -92,7 +94,7 @@ def simulate(data, truck_volume, deviation, site_operation, shuttle, plant_opera
                                                 route_count += 1
                                                 route_num = route_count
                                                 trip_round = 3
-                                                utilization = route_volume / shift / (truck_volume / 0.7 * trip_round)  # 还原到卡车理论容积再运算
+                                                utilization = route_volume / shift / (truck_volume / loading_rate * trip_round)  # 还原到卡车理论容积再运算
 
                                                 result.append([plant, region, route_num, site, site_volume, route_volume, utilization, trip_round, trip_time])
                                                 result.append([plant, region, route_num, i, i_volume, route_volume, utilization, trip_round, trip_time])
@@ -121,7 +123,7 @@ def simulate(data, truck_volume, deviation, site_operation, shuttle, plant_opera
                                                     route_count += 1
                                                     route_num = route_count
                                                     trip_round = 2
-                                                    utilization = route_volume / shift / (truck_volume / 0.7 * trip_round)  # 还原到卡车理论容积再运算
+                                                    utilization = route_volume / shift / (truck_volume / loading_rate * trip_round)  # 还原到卡车理论容积再运算
 
                                                     result.append([plant, region, route_num, site, site_volume, route_volume, utilization, trip_round, trip_time])
                                                     result.append([plant, region, route_num, i, i_volume, route_volume, utilization, trip_round, trip_time])
@@ -140,14 +142,15 @@ def simulate(data, truck_volume, deviation, site_operation, shuttle, plant_opera
 
 def main():
     data = pd.read_excel('Template.xlsx')
-    truck_volume = 9.6 * 2.35 * 2.4 * 0.7
+    loading_rate = 0.65
+    truck_volume = 9.6 * 2.35 * 2.4 * loading_rate
     deviation = 0.05
     site_operation = 0.25 + 0.25
     shuttle = 0.25
     plant_operation = 0.25 + 0.5
     speed_close = 25
     speed_far = 40
-    simulate(data, truck_volume, deviation, site_operation, shuttle, plant_operation, speed_close, speed_far)
+    simulate(data, truck_volume, loading_rate, deviation, site_operation, shuttle, plant_operation, speed_close, speed_far)
 
 
 if __name__ == '__main__':
